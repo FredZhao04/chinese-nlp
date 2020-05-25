c259b7a2568755c9dfad592cfd0b1ae043200f61
zhaoxining <zhaoxining@higinet.com.cn>
2020-05-25 16:50:15 +0800

    修改minhash测试代码
    修改minhash测试代码，分别计算全样本、正样本、负样本的识别准确率

--- SentenceSimilarity/examples/sim_minhash_test.py
+++ SentenceSimilarity/examples/sim_minhash_test.py
@@ -13,25 +13,30 @@ if __name__=="__main__":
     train_data = pd.read_csv('../data/sim_sentence_train.csv')
     M = len(train_data)
     N = 0
+    total = 0
+    simer = SimJaccard()
     for i in range(M):
         sentence1 = train_data['sentence1'][i]
         sentence2 = train_data['sentence2'][i]
         lable = train_data['label'][i]
-
-        simer = SimJaccard()
-        sim = simer.distance(sentence1, sentence2)
-        if sim >= 0.5:
-            sim = 1
-        else:
-            sim = 0
-
-        if sim == lable:
-            N += 1
+        if lable == 0:
+            total += 1
+            sim = simer.distance(sentence1, sentence2)
+            if sim < 0.4:
+                N += 1
+        # sim = simer.distance(sentence1, sentence2)
+        # if sim >= 0.5:
+        #     sim = 1
+        # else:
+        #     sim = 0
+        #
+        # if sim == lable:
+        #     N += 1
 
         if i % 1000 == 0:
             print("************程序运行到 %d 条**************" % i)
-
-    print("准确率为: %f " % (N/M))
+    # print("准确率为: %f " % (N /M))
+    print("准确率为: %f " % (N/total))
 
     end_time = time()
     print('该程序运行时间：',(end_time - begin_time))
