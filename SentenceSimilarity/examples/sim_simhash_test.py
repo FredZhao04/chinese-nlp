02017c7173357047029a3c7c1daaea094048b0a0
zhaoxining <zhaoxining@higinet.com.cn>
2020-05-26 11:03:38 +0800

    revised simhash test
    revised simhash test

--- SentenceSimilarity/examples/sim_simhash_test.py
+++ SentenceSimilarity/examples/sim_simhash_test.py
@@ -13,20 +13,28 @@ if __name__=="__main__":
     train_data = pd.read_csv('../data/sim_sentence_train.csv')
     M = len(train_data)
     N = 0
+    total = 0
+    simer = SimHaming()
     for i in range(M):
         sentence1 = train_data['sentence1'][i]
         sentence2 = train_data['sentence2'][i]
         lable = train_data['label'][i]
+        # sim = simer.distance(sentence1, sentence2)
+        # 测试负样本准确率
+        # if lable == 0:
+        #     total += 1
+        #     if sim < 0.5:
+        #         N += 1
+        # 测试正样本准确率
+        if lable == 1:
+            total += 1
+            sim = simer.distance(sentence1, sentence2)
+            if sim >= 0.5:
+                N += 1
 
-        simer = SimHaming()
-        sim = simer.distance(sentence1, sentence2)
-        if sim >= 0.5:
-            sim = 1
-        else:
-            sim = 0
-
-        if sim == lable:
-            N += 1
+        # 总体准确率
+        # if (sim >= 0.5 and lable == 1) or (sim < 0.5 and lable == 0):
+        #     N += 1
 
         if i % 1000 == 0:
             print("************程序运行到 %d 条**************" % i)
