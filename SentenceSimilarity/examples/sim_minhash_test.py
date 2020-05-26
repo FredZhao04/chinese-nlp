aa5f9379c80b430e7c7bad6cc2cde04be42b7051
zhaoxining <zhaoxining@higinet.com.cn>
2020-05-26 11:08:07 +0800

    revised minhash test
    revised minhash test

--- SentenceSimilarity/examples/sim_minhash_test.py
+++ SentenceSimilarity/examples/sim_minhash_test.py
@@ -19,24 +19,20 @@ if __name__=="__main__":
         sentence1 = train_data['sentence1'][i]
         sentence2 = train_data['sentence2'][i]
         lable = train_data['label'][i]
-        if lable == 0:
-            total += 1
-            sim = simer.distance(sentence1, sentence2)
-            if sim < 0.4:
-                N += 1
-        # sim = simer.distance(sentence1, sentence2)
-        # if sim >= 0.5:
-        #     sim = 1
-        # else:
-        #     sim = 0
-        #
-        # if sim == lable:
-        #     N += 1
+        # if lable == 0:
+        #     total += 1
+        #     sim = simer.distance(sentence1, sentence2)
+        #     if sim < 0.4:
+        #         N += 1
+        sim = simer.distance(sentence1, sentence2)
+        # 测试总体准确率
+        if (sim >= 0.5 and lable == 1) or (sim < 0.5 and lable == 0):
+            N += 1
 
         if i % 1000 == 0:
             print("************程序运行到 %d 条**************" % i)
-    # print("准确率为: %f " % (N /M))
-    print("准确率为: %f " % (N/total))
+    print("准确率为: %f " % (N /M))
+    # print("准确率为: %f " % (N/total))
 
     end_time = time()
     print('该程序运行时间：',(end_time - begin_time))
