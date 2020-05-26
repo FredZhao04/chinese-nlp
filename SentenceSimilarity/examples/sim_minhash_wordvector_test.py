20039e29b8d3e5297bd3a12ceb23976e8d119ac6
zhaoxining <zhaoxining@higinet.com.cn>
2020-05-26 16:32:27 +0800

    revised minhash_wordvector test
    revised minhash_wordvector test

--- SentenceSimilarity/examples/sim_minhash_wordvector_test.py
+++ SentenceSimilarity/examples/sim_minhash_wordvector_test.py
@@ -26,22 +26,24 @@ if __name__=="__main__":
         sentence1 = train_data['sentence1'][i]
         sentence2 = train_data['sentence2'][i]
         lable = train_data['label'][i]
-        if lable == 0:
-            total += 1
-            sim1 = simer_minhash.distance(sentence1, sentence2)
-            sim2 = simer_wordvector.distance(sentence1, sentence2)
-            sim = sim1 * 0.2 + sim2 * 0.8
-            if sim < 0.5:
-                N += 1
-        # sim1 = simer_minhash.distance(sentence1, sentence2)
-        # sim2 = simer_wordvector.distance(sentence1, sentence2)
-        # sim = sim1*0.2 + sim2*0.8
-        # if sim >= 0.5:
-        #     N += 1
+        # 负样本
+        # if lable == 0:
+        #     total += 1
+        #     sim1 = simer_minhash.distance(sentence1, sentence2)
+        #     sim2 = simer_wordvector.distance(sentence1, sentence2)
+        #     sim = sim1 * 0.2 + sim2 * 0.8
+        #     if sim < 0.5:
+        #         N += 1
+
+        sim1 = simer_minhash.distance(sentence1, sentence2)
+        sim2 = simer_wordvector.distance(sentence1, sentence2)
+        sim = sim1*0.5 + sim2*0.5
+        if (sim >= 0.5 and lable == 1) or (sim < 0.5 and lable == 0):
+            N += 1
         if i % 1000 == 0:
             print("************程序运行到 %d 条**************" % i)
 
-    print("准确率为: %f " % (N/total))
+    print("准确率为: %f " % (N/M))
 
     end_time = time()
     print('该程序运行时间：',(end_time - begin_time))
